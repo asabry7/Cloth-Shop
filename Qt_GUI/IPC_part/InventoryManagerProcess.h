50e8794f00987158a08a408d8b730ffe90271d1d
asabry7 <aaly6995@gmail.com>
2025-03-01 02:52:53 +0200

    remove test file

--- Qt_GUI/IPC_part/InventoryManagerProcess.h
+++
@@ -1,9 +0,0 @@
-#ifndef INVENTORY_MANAGER_PROCESS_H
-#define INVENTORY_MANAGER_PROCESS_H
-#define _GNU_SOURCE
-
-#include "SharedData.h"
-
-void StartInventoryManagerProcess(Cloth_t *shared_data, PidStorage_t *pid_storage);
-
-#endif
